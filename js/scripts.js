a4b8a05e945e77240388b54875d1eee9eb7d85d4
Nathan Banks <nathan.banks@ntlworld.com>
2018-03-04 13:06:54 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,10 +24,7 @@ $(document).ready(function() {
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
   convertToMB();
-  $.get('file_to_read.txt', function(data) {
-   var log = data;
-  }, 'text');
-  alert(log);
+  
 });
 
 function convertToMB(){
